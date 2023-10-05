281a837750846134500a6e55c9d3cb95dd4c57ea
chadeqmartin <chadeqmartin@gmail.com>
2023-10-05 17:26:04 -0400

    updated with extra solution

--- factorial.py
+++ factorial.py
@@ -1,3 +1,14 @@
 def factorial(num):
-	# your code here
-	pass
+	# if num == 0:
+	# 	return 1
+	# else:
+	# 	return num * factorial(num - 1)
+
+	product = 1
+
+	while num > 0:
+		product *= num
+		num -= 1
+		
+	return product
+
